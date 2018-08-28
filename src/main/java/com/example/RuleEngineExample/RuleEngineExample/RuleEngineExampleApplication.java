80887c3ab420164ec2d3410815385997ae839541
sercankulig <sercan.kulig@krontech.com>
2018-08-28 11:54:49 +0300

    start

---
+++ src/main/java/com/example/RuleEngineExample/RuleEngineExample/RuleEngineExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.RuleEngineExample.RuleEngineExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEngineExampleApplication.class, args);
+	}
+}
